cddf67679aab7f8d34f00260e25d85fa02156556
Jaberkro <jaberkro@student.codam.nl>
2022-02-24 23:26:32 +0100

    functions to rotate a doubly linked list

---
+++ ft_dlstrev_rotate.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_dlstrrotate.c                                   :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/02/24 19:56:08 by jaberkro      #+#    #+#                 */
+/*   Updated: 2022/02/24 20:03:27 by jaberkro      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_dlstrev_rotate(t_dlist **dlst)
+{
+	t_dlist *copy;
+	t_dlist *tmp;
+
+	tmp = *dlst;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	copy = tmp->previous;
+	tmp->next = *dlst;
+	tmp->next->previous = tmp;
+	copy->next->previous = NULL;
+	*dlst = copy->next;
+	copy->next = NULL;
+}
