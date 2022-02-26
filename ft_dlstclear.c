3c4f70f304ecba36f028c5809af496ad64071d28
Jaberkro <jaberkro@student.codam.nl>
2022-02-26 16:23:26 +0100

    created function to free a doubly linked list

---
+++ ft_dlstclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_dlstclear.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/02/25 18:37:48 by jaberkro      #+#    #+#                 */
+/*   Updated: 2022/02/26 15:41:27 by jaberkro      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void    ft_dlstclear(t_dlist **dlst)
+{
+    t_dlist	*tmp;
+
+	if (!dlst)
+		return ;
+	while (*dlst)
+	{
+		tmp = *dlst;
+		*dlst = (*dlst)->next;
+        free(tmp);
+	}
+}
