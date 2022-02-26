592a749dadca14d3b5101f644871256fe398c63f
Jaberkro <jaberkro@student.codam.nl>
2022-02-26 16:22:23 +0100

    removed outcommented old code

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/22 20:59:15 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/02/26 16:13:44 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/02/26 16:21:59 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,47 +37,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	}
 	return (out);
 }
-
-// char	*ft_strjoin(char const *s1, char const *s2)
-// {
-// 	int		len;
-// 	char	*out;
-// 	size_t	i;
-
-// 	i = 0;
-// 	if (!s1 && !s2)
-// 		return (NULL);
-// 	if (!s1)
-// 		return ((char *)s2);
-// 	// if (!s2)
-// 	// 	return ((char *)s1);
-// 	len = ft_strlen(s1) + ft_strlen(s2);
-// 	out = ft_calloc(len + 1, 1);
-// 	if (out == NULL)
-// 		return (NULL);
-// 	while (*s1)
-// 	{
-// 		*out = *s1;
-// 		out++;
-// 		s1++;
-// 	}
-// 	while (*s2)
-// 	{
-// 		*out = *s2;
-// 		out++;
-// 		s2++;
-// 	}
-// 	return (out - len);
-// }
-	//free ((char *)s1);
-	// while (i < ft_strlen(s1) + ft_strlen(s2))
-	// {
-	// 	if (i < ft_strlen(s1))
-	// 		out[i] = s1[i];
-	// 	else
-	// 		out[i] = s2[i - ft_strlen(s1)];
-	// 	i++;
-	// }
 
 /* Allocates (with malloc(3)) and returns a new
 string, which is the result of the concatenation
