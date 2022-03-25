0483149c74a581716cefef2dc656196cd5f51cb9
Jaberkro <jaberkro@student.codam.nl>
2022-03-25 11:30:00 +0100

    not norm but will not be used either

--- src/sorting/radix_sort.c
+++ src/sorting/radix_sort.c
@@ -6,220 +6,159 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/24 12:21:14 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/03/25 10:57:25 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/03/25 11:29:46 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int present(t_dlist *stack, int decimal, int tofind)
+int	present(t_dlist *stack, int decimal, int tofind)
 {
-    while (stack)
-    {
-        if ((stack->val) / decimal % 10 == tofind)
-            return (1);
-        stack = stack->next;
-    }
-    return (0);
+	while (stack)
+	{
+		if ((stack->val) / decimal % 10 == tofind)
+			return (1);
+		stack = stack->next;
+	}
+	return (0);
 }
 
-void    rotate_back(t_dlist **stack, int rotate_count, int len, int ab)
+void	rotate_back(t_dlist **stack, int rotate_count, int len, int ab)
 {
-    if (rotate_count > len / 2)
-    {
-        while (rotate_count < len)
-        {
-            if (ab == 0)
-                ps_ra(stack);
-            else
-                ps_rb(stack);
-            rotate_count++;
-        }
-    }
-    else
-    {
-        while (rotate_count > 0)
-        {
-            if (ab == 0)
-                ps_rra(stack);
-            else
-                ps_rrb(stack);
-            rotate_count--;
-        }
-    }
+	if (rotate_count > len / 2)
+	{
+		while (rotate_count < len)
+		{
+			if (ab == 0)
+				ps_ra(stack);
+			else
+				ps_rb(stack);
+			rotate_count++;
+		}
+	}
+	else
+	{
+		while (rotate_count > 0)
+		{
+			if (ab == 0)
+				ps_rra(stack);
+			else
+				ps_rrb(stack);
+			rotate_count--;
+		}
+	}
 }
 
-int find_closest(t_dlist *stack, int decimal, int i)
+int	find_closest(t_dlist *stack, int decimal, int i)
 {
-    int     forward;
-    int     backward;
-    t_dlist *tmp;
+	int		forward;
+	int		backward;
+	t_dlist	*tmp;
 
-    forward = 0;
-    backward = 1;
-    tmp = ft_dlstlast(stack);
-    if (present(stack, decimal, i) == 0)
-        return (1000);
-    while (tmp)
-    {
-        if (tmp->val / decimal % 10 == i)
-            break;
-        tmp = tmp->previous;
-        backward++;
-    }
-    while (stack)
-    {
-        if (stack->val / decimal % 10 == i)
-            break;
-        stack = stack->next;
-        forward++;
-    }
-    if (forward < backward)
-        return (forward);
-    return (backward);
+	forward = 0;
+	backward = 1;
+	tmp = ft_dlstlast(stack);
+	if (present(stack, decimal, i) == 0)
+		return (1000);
+	while (tmp)
+	{
+		if (tmp->val / decimal % 10 == i)
+			break ;
+		tmp = tmp->previous;
+		backward++;
+	}
+	while (stack)
+	{
+		if (stack->val / decimal % 10 == i)
+			break ;
+		stack = stack->next;
+		forward++;
+	}
+	if (forward < backward)
+		return (forward);
+	return (backward);
 }
 
-void    sort_descending(t_dlist **stack_a, t_dlist **stack_b, int decimal)
+void	sort_descending(t_dlist **stack_a, t_dlist **stack_b, int decimal)
 {
-    int i;
-    int j;
-    int len;
-    int rotate_count;
+	int	i;
+	int	j;
+	int	len;
+	int	rotate_count;
 
-    i = 0;
-    while (i < 10)
-    {
-        rotate_count = 0;
-        len = ft_dlstlen(*stack_a);
-        j = 0;
-        while (j < len)
-        {
-            if (present(*stack_a, decimal, i) == 0 && rotate_count > 0)
-            {
-                rotate_back(stack_a, rotate_count, ft_dlstlen(*stack_a), 0);
-                break;
-            }
-            if (*stack_a && (((*stack_a)->val) / decimal) % 10 == i)
-                ps_pb(stack_b, stack_a);
-            else if (*stack_a)
-            {
-                ps_ra(stack_a);
-                rotate_count++;
-            }
-            j++;
-        }
-        i++;
-    }
+	i = 0;
+	while (i < 10)
+	{
+		rotate_count = 0;
+		len = ft_dlstlen(*stack_a);
+		j = 0;
+		while (j < len)
+		{
+			if (present(*stack_a, decimal, i) == 0 && rotate_count > 0)
+			{
+				rotate_back(stack_a, rotate_count, ft_dlstlen(*stack_a), 0);
+				break ;
+			}
+			if (*stack_a && (((*stack_a)->val) / decimal) % 10 == i)
+				ps_pb(stack_b, stack_a);
+			else if (*stack_a)
+			{
+				ps_ra(stack_a);
+				rotate_count++;
+			}
+			j++;
+		}
+		i++;
+	}
 }
 
-void    sort_ascending(t_dlist **stack_a, t_dlist **stack_b, int decimal)
+void	sort_ascending(t_dlist **stack_a, t_dlist **stack_b, int decimal)
 {
-    int i;
-    int j;
-    int len;
-    int rotate_count;
+	int	i;
+	int	j;
+	int	len;
+	int	rotate_count;
 
-    i = 9;
-    while (i >= 0)
-    {
-        rotate_count = 0;
-        len = ft_dlstlen(*stack_b);
-        j = 0;
-        while (j < len)
-        {
-            if (present(*stack_b, decimal, i) == 0 && rotate_count > 0)
-            {
-                rotate_back(stack_b, rotate_count, ft_dlstlen(*stack_b), 1);
-                break;
-            }
-            if (*stack_b && (((*stack_b)->val) / decimal) % 10 == i)
-                ps_pa(stack_a, stack_b);
-            else if (*stack_b)
-            {
-                ps_rb(stack_b);
-                rotate_count++;
-            }
-            j++;
-        }
-        i--;
-    }
+	i = 9;
+	while (i >= 0)
+	{
+		rotate_count = 0;
+		len = ft_dlstlen(*stack_b);
+		j = 0;
+		while (j < len)
+		{
+			if (present(*stack_b, decimal, i) == 0 && rotate_count > 0)
+			{
+				rotate_back(stack_b, rotate_count, ft_dlstlen(*stack_b), 1);
+				break ;
+			}
+			if (*stack_b && (((*stack_b)->val) / decimal) % 10 == i)
+				ps_pa(stack_a, stack_b);
+			else if (*stack_b)
+			{
+				ps_rb(stack_b);
+				rotate_count++;
+			}
+			j++;
+		}
+		i--;
+	}
 }
 
-// void    sort_ascending(t_dlist **stack_a, t_dlist **stack_b, int decimal)
-// {
-//     int i;
-//     int closest;
-
-//     i = 9;
-//     while (i >= 0)
-//     {
-//         closest = find_closest(*stack_b, decimal, i);
-//         if (closest == 1000)
-//             continue ;
-//         else
-//         {
-//             while (closest > 0)
-//             {
-//                 ps_rb(stack_b);
-//             }
-//             while (closest < 0)
-//             {
-//                 ps_rrb(stack_b);
-//             }
-//         }
-//         ps_pa(stack_a, stack_b);
-//         i--;
-//     }
-// }
-
-// void    sort_descending(t_dlist **stack_a, t_dlist **stack_b, int decimal)
-// {
-//     int i;
-//     int closest;
-
-//     i = 0;
-//     while (i < 10)
-//     {
-//         closest = find_closest(*stack_a, decimal, i);
-//         if (closest == 1000)
-//             continue ;
-//         else
-//         {
-//             while (closest > 0)
-//             {
-//                 ps_ra(stack_a);
-//                 closest--;
-//             }
-//             while (closest < 0)
-//             {
-//                 ps_rra(stack_a);
-//                 closest++;
-//             }
-//         }
-//         ps_pb(stack_b, stack_a);
-//         i++;
-//     }
-// }
-
-// void    sort(t_dlist **stack_to, t_dlist **stack_from, int ascending, int decimal)
-// {
-
-// }
-
-void    radix_sort(t_dlist **stack_a, int total)
+void	radix_sort(t_dlist **stack_a, int total)
 {
-    int     decimal;
-    t_dlist *stack_b;
+	int		decimal;
+	t_dlist	*stack_b;
 
-    decimal = 1;
-    stack_b = NULL;
-    while (decimal <= total)
-    {
-        if (*stack_a && check_sorted(*stack_a, 1) == 1)
-            return ;
-        sort_descending(stack_a, &stack_b, decimal);
-        decimal *= 10;
-        sort_ascending(stack_a, &stack_b, decimal);
-        decimal *= 10;
-    }
+	decimal = 1;
+	stack_b = NULL;
+	while (decimal <= total)
+	{
+		if (*stack_a && check_sorted(*stack_a, 1) == 1)
+			return ;
+		sort_descending(stack_a, &stack_b, decimal);
+		decimal *= 10;
+		sort_ascending(stack_a, &stack_b, decimal);
+		decimal *= 10;
+	}
 }
